b907a58b166aa2012ebc95bcec9611342d7ba7fe
CharlyDotCom <52584676+CharlyDotCom@users.noreply.github.com>
2022-12-11 12:09:03 +0100

    Modelo definido

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.Index),
     path('about/', views.About),
-    path('hello/<str:username>', views.Hello)
+    path('hello/<str:username>', views.Hello),
+    path('projects/', views.Projects),
+    path('tasks/<int:id>', views.Tasks)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,24 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404    
 # Create your views here.
 
 def Index(request):
     return HttpResponse("<h1>Index Page</h1>")
 
 def Hello(request, username):
+    print(type(username))
     print(username)
     return HttpResponse("<h1>Hello %s </h1>" % username)
 
 def About(request):
     return HttpResponse("<h3>About</h3>")
+
+def Projects(request):
+    prjs = list(Project.objects.values())
+    return JsonResponse(prjs, safe=False);
+
+def Tasks(request, id):
+    # tas = Task.objects.get(id=id)
+    tas = get_object_or_404(Task, id=id)
+    return HttpResponse("<h3>Tasks: %s </h3>" % tas.title);
